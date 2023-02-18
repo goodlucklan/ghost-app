4bcc92b6f2629f1c8fc0b8955972b5f5580edb89
unknown <goodluckxdc@gmail.com>
2023-02-17 22:22:50 -0500

    navbar

--- components/UI/NavBar.tsx
+++ components/UI/NavBar.tsx
@@ -1,7 +1,40 @@
-import React from 'react'
-
+import Image from "next/image";
+import NextLink from "next/link";
+import { Spacer, Text, useTheme, Link } from "@nextui-org/react";
+import "./Navbar.style";
 export const NavBar = () => {
+  const { theme } = useTheme();
   return (
-    <div>NavBar</div>
-  )
-}
+    <div
+      className="nav"
+      style={{
+        backgroundColor: theme?.colors.gray900.value,
+      }}
+    >
+      <Image
+        src="https://i.imgur.com/CMKVhKi.png"
+        alt="Bienvenido a GhostGunz"
+        width={70}
+        height={70}
+      />
+      <NextLink href="/" passHref>
+        <Link>
+          <Text color="white" h2>
+            P
+          </Text>
+          <Text color="white" h3>
+            okémon!
+          </Text>
+        </Link>
+      </NextLink>
+
+      <Spacer css={{ flex: 1 }} />
+
+      <NextLink href="/favorites" passHref>
+        <Link css={{ marginRight: "10px" }}>
+          <Text color="white">Favoritos</Text>
+        </Link>
+      </NextLink>
+    </div>
+  );
+};
